8db024e83fbfdf56b22c176b640fe16c010b0ce7
Dima <D130188@yandex.ru>
2023-07-26 21:44:17 +0400

    added db host

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
-const { HTTP_STATUS_INTERNAL_SERVER_ERROR } = require('node:http2').constants;
 const cors = require('cors');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const routes = require('./routes/index');
+const { handleErrors } = require('./utils/handleErrors');
 
-const { PORT = 3000 } = process.env;
+const { NODE_ENV, PORT = 3000, DB_HOST } = process.env;
 
 const app = express();
 
 app.use(cookieParser());
 
-mongoose.connect('mongodb://0.0.0.0:27017/bitfilmsdb', {
+mongoose.connect(NODE_ENV === 'production' ? DB_HOST : 'mongodb://0.0.0.0:27017/bitfilmsdb', {
   useNewUrlParser: true,
 }).then(() => {
   console.log('База данных успешно подключена.');
@@ -34,19 +34,7 @@ app.use(errorLogger);
 
 app.use(errors());
 
-app.use((err, req, res, next) => {
-  const { statusCode = HTTP_STATUS_INTERNAL_SERVER_ERROR, message } = err;
-
-  res
-    .status(statusCode)
-    .send({
-      message: statusCode === HTTP_STATUS_INTERNAL_SERVER_ERROR
-        ? 'На сервере произошла ошибка.'
-        : message,
-    });
-
-  next();
-});
+app.use(handleErrors);
 
 app.listen(PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`);
